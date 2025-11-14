46a5d816b419c0432777a1e37b92cd77b7e13023
Safin178 <redwanulhoque37@gmail.com>
2025-11-14 13:30:12 +0600

    compare func added

---
+++ Basics/compare.cpp
@@ -0,0 +1,18 @@
+bool cmp(pair<int, int> A, pair<int, int> B) // pair compare
+{
+    if (A.first == B.first)
+        return (A.second > B.second);
+    //! if A.second is Larger than A will come before B
+    return (A.first < B.first);
+    //! if A.first is smaller than A will come before B
+}
+
+
+
+int double_comp(ld a, ld b) //compare between 2 double
+{
+    if (fabs(a - b) <= eps)
+        return 0;
+    return a < b ? -1 : 1;
+    //! dbl cmp return 0 when a==b, -1 if a<b ,, 1 if a>b
+}
