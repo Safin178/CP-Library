719f92ba8772054cf358f20f5b1b13b91b88ba1a
Afrin Hasan Safin <redwanulhoque37@gmail.com>
2026-05-28 19:25:45 +0600

    hashing template added

---
+++ String/hashing.cpp
@@ -0,0 +1,62 @@
+const int nmax = 1e6 + 9;
+
+const ll prime1 = 1e9 + 7;
+const ll prime2 = 1e9 + 9;
+
+const ll BASE1 = 1000003;
+const ll BASE2 = 10000019;
+
+pair<ll,ll> power[nmax];
+
+void precal() {
+    power[0] = {1, 1};
+
+    for(int i = 1; i < nmax; i++) {
+        power[i].first = (power[i - 1].first * BASE1) % prime1;
+        power[i].second = (power[i - 1].second * BASE2) % prime2;
+    }
+}
+
+struct Hashing {
+
+    string s;
+    int n;
+
+    vector<pair<ll,ll>> H;
+
+    Hashing() {}
+
+    Hashing(string _s) {
+        s = _s;
+        n = s.size();
+
+        H.resize(n);
+
+        H[0] = {s[0], s[0]};
+
+        for(int i = 1; i < n; i++) {
+
+            H[i].first = (H[i - 1].first * BASE1 + s[i]) % prime1;
+
+            H[i].second = (H[i - 1].second * BASE2 + s[i]) % prime2;
+        }
+    }
+
+    pair<ll,ll> get_hash(int l, int r) {
+        // 0-indexed
+
+        pair<ll,ll> ret;
+
+        if(l == 0) {
+            return H[r];
+        }
+
+        ret.first = (H[r].first - H[l - 1].first * power[r - l + 1].first) % prime1;
+        if(ret.first < 0) ret.first += prime1;
+
+        ret.second = (H[r].second - H[l - 1].second * power[r - l + 1].second) % prime2;
+        if(ret.second < 0) ret.second += prime2;
+
+        return ret;
+    }
+};
